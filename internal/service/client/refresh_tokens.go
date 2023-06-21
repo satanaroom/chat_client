0455b7081c8c5ab37a922e3e48f2c943fc9edfa3
Yan Karpov <ftragula@mail.ru>
2023-06-21 16:08:14 +0300

    Fixed refresh tokens method

--- internal/service/client/refresh_tokens.go
+++ internal/service/client/refresh_tokens.go
@@ -13,90 +13,100 @@ func (s *service) RefreshTokens(ctx context.Context, refreshTokenPeriod, accessT
 	go func() {
 		t := time.NewTicker(refreshTokenPeriod)
 
-		for _ = range t.C {
-			loggedUsername, err := s.redisClient.Get(ctx, model.LoggedUsername)
-			if err != nil {
-				logger.Errorf("failed to get logged username: %s", err.Error())
-				continue
+		for {
+			select {
+			case <-t.C:
+				loggedUsername, err := s.redisClient.Get(ctx, model.LoggedUsername)
+				if err != nil {
+					logger.Errorf("failed to get logged username: %s", err.Error())
+					continue
+				}
+
+				tokens, err := s.redisClient.Get(ctx, loggedUsername)
+				if err != nil {
+					logger.Errorf("failed to get logged username tokens: %s", err.Error())
+					continue
+				}
+
+				var userTokens model.UserTokens
+				if err = json.Unmarshal([]byte(tokens), &userTokens); err != nil {
+					logger.Errorf("unmarshal tokens: %s", err.Error())
+					continue
+				}
+
+				newRefreshToken, err := s.authClient.UpdateRefreshToken(ctx, userTokens.RefreshToken)
+				if err != nil {
+					logger.Errorf("failed to get new refresh token: %s", err.Error())
+					continue
+				}
+
+				userTokens.RefreshToken = newRefreshToken
+
+				newTokens, err := json.Marshal(userTokens)
+				if err != nil {
+					logger.Errorf("marshal refreshed tokens: %s", err.Error())
+					continue
+				}
+
+				if err = s.redisClient.Set(ctx, loggedUsername, string(newTokens), 0); err != nil {
+					logger.Errorf("failed to set new tokens: %s", err.Error())
+					continue
+				}
+
+				logger.Info("access token has been updated")
+			case <-ctx.Done():
+				return
 			}
-
-			tokens, err := s.redisClient.Get(ctx, loggedUsername)
-			if err != nil {
-				logger.Errorf("failed to get logged username tokens: %s", err.Error())
-				continue
-			}
-
-			var userTokens model.UserTokens
-			if err = json.Unmarshal([]byte(tokens), &userTokens); err != nil {
-				logger.Errorf("unmarshal tokens: %s", err.Error())
-				continue
-			}
-
-			newRefreshToken, err := s.authClient.UpdateRefreshToken(ctx, userTokens.RefreshToken)
-			if err != nil {
-				logger.Errorf("failed to get new refresh token: %s", err.Error())
-				continue
-			}
-
-			userTokens.RefreshToken = newRefreshToken
-
-			newTokens, err := json.Marshal(userTokens)
-			if err != nil {
-				logger.Errorf("marshal refreshed tokens: %s", err.Error())
-				continue
-			}
-
-			if err = s.redisClient.Set(ctx, loggedUsername, string(newTokens), 0); err != nil {
-				logger.Errorf("failed to set new tokens: %s", err.Error())
-				continue
-			}
-
-			logger.Info("access token has been updated")
 		}
 	}()
 
 	go func() {
 		t := time.NewTicker(accessTokenPeriod)
 
-		for _ = range t.C {
-			loggedUsername, err := s.redisClient.Get(ctx, model.LoggedUsername)
-			if err != nil {
-				logger.Errorf("failed to get logged username: %s", err.Error())
-				continue
+		for {
+			select {
+			case <-t.C:
+				loggedUsername, err := s.redisClient.Get(ctx, model.LoggedUsername)
+				if err != nil {
+					logger.Errorf("failed to get logged username: %s", err.Error())
+					continue
+				}
+
+				tokens, err := s.redisClient.Get(ctx, loggedUsername)
+				if err != nil {
+					logger.Errorf("failed to get logged username tokens: %s", err.Error())
+					continue
+				}
+
+				var userTokens model.UserTokens
+				if err = json.Unmarshal([]byte(tokens), &userTokens); err != nil {
+					logger.Errorf("unmarshal tokens: %s", err.Error())
+					continue
+				}
+
+				newAccessToken, err := s.authClient.GetAccessToken(ctx, userTokens.RefreshToken)
+				if err != nil {
+					logger.Errorf("failed to get new access token: %s", err.Error())
+					continue
+				}
+
+				userTokens.AccessToken = newAccessToken
+
+				newTokens, err := json.Marshal(userTokens)
+				if err != nil {
+					logger.Errorf("marshal refreshed tokens: %s", err.Error())
+					continue
+				}
+
+				if err = s.redisClient.Set(ctx, loggedUsername, string(newTokens), 0); err != nil {
+					logger.Errorf("failed to set new tokens: %s", err.Error())
+					continue
+				}
+
+				logger.Info("access token has been updated")
+			case <-ctx.Done():
+				return
 			}
-
-			tokens, err := s.redisClient.Get(ctx, loggedUsername)
-			if err != nil {
-				logger.Errorf("failed to get logged username tokens: %s", err.Error())
-				continue
-			}
-
-			var userTokens model.UserTokens
-			if err = json.Unmarshal([]byte(tokens), &userTokens); err != nil {
-				logger.Errorf("unmarshal tokens: %s", err.Error())
-				continue
-			}
-
-			newAccessToken, err := s.authClient.GetAccessToken(ctx, userTokens.RefreshToken)
-			if err != nil {
-				logger.Errorf("failed to get new access token: %s", err.Error())
-				continue
-			}
-
-			userTokens.AccessToken = newAccessToken
-
-			newTokens, err := json.Marshal(userTokens)
-			if err != nil {
-				logger.Errorf("marshal refreshed tokens: %s", err.Error())
-				continue
-			}
-
-			if err = s.redisClient.Set(ctx, loggedUsername, string(newTokens), 0); err != nil {
-				logger.Errorf("failed to set new tokens: %s", err.Error())
-				continue
-			}
-
-			logger.Info("access token has been updated")
 		}
 	}()
 }
